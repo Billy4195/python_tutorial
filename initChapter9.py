ae41bacaf8e8989001e80048411d96529b7381b2
Billy4195 <g4691821@gmail.com>
2016-02-20 17:52:44 +0800

    add initchapter9

---
+++ initChapter9.py
@@ -0,0 +1,26 @@
+import json
+
+def section1():
+    section1 = []
+    section1.append('Function can simlify programming when some code is frequently used')
+    section1.append('put code into function, then simply call the fuction is rather \nthan copy the same code')
+    section1.append('The following is syntax')
+    section1.append('def function_name(argument):')
+    section1.append('    ---function code---')
+    section1.append('example:')
+    section1.append('>>>def print_num(i):')
+    section1.append('>>>    print(i)')
+    section1.append('>>>print_num(5)')
+    section1.append('>>>5')
+    section1.append('>>>print_num(199)')
+    section1.append('>>>199')
+    return section1
+    
+def initChapter9(fileName):
+    chapter9 = []
+    chapter9.append(section1())
+    try:
+        with open(fileName,'w') as chapter9File:
+            chapter9File.write(json.dumps(chapter9))
+    except IOError:
+        print(fileName+' open failed')

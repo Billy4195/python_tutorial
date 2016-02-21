3f57a1dc4ea1355736ee0475610606373c4e3016
Billy4195 <g4691821@gmail.com>
2016-02-21 23:21:41 +0800

    setChapters to call all initFile

---
+++ setChapters.py
@@ -0,0 +1,31 @@
+import os
+import initChapter1
+import initChapter2
+import initChapter3
+import initChapter4
+import initChapter5
+import initChapter6
+import initChapter7
+import initChapter8
+import initChapter9
+from initChaptersFile import initChaptersFile
+initChaptersFile()
+initChapter1.initChapter1('chapters.txt')
+print('Successfully create chapter1')
+initChapter2.initChapter2('chapters.txt')
+print('Successfully create chapter2')
+initChapter3.initChapter3('chapters.txt')
+print('Successfully create chapter3')
+initChapter4.initChapter4('chapters.txt')
+print('Successfully create chapter4')
+initChapter5.initChapter5('chapters.txt')
+print('Successfully create chapter5')
+initChapter6.initChapter6('chapters.txt')
+print('Successfully create chapter6')
+initChapter7.initChapter7('chapters.txt')
+print('Successfully create chapter7')
+initChapter8.initChapter8('chapters.txt')
+print('Successfully create chapter8')
+initChapter9.initChapter9('chapters.txt')
+print('Successfully create chapter9')
+os.system('pause')

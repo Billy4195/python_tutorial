febbf669bbcdb284a4a2fda4a75b783d9dd8f07a
Billy4195 <g4691821@gmail.com>
2016-02-22 21:29:21 +0800

    load section to section class

--- chapter.py
+++ chapter.py
@@ -1,7 +1,10 @@
+from section import section
 class chapter:
     def __init__(self,jsonchapter):
         self.chapterName = jsonchapter['chapterName']
         self.chapterNum = jsonchapter['chapterNum']
         self.sectionName = jsonchapter['sectionName']
-    def setSection():
-        pass
+    def setSection(self,introductions):
+        self.sections = []
+        for index in range(0,len(introductions)):
+            self.sections.append(section(introductions[index]))
--- loadChapterFile.py
+++ loadChapterFile.py
@@ -4,10 +4,13 @@ from chapter import chapter
 def loadChapterFile(fileName):
     try:
         with open(fileName,'r') as chapterFile:
-            jsonchapter = json.loads(chapterFile.read())
+            lines = chapterFile.readlines()
+            jsonchapter = json.loads(lines[0])
             chapterContainer = []
             for index in jsonchapter:
                 chapterContainer.append( chapter(index) )
+            for line,chapter_ in zip(lines[1:],chapterContainer):
+                chapter_.setSection(json.loads(line))
             return chapterContainer
     except IOError:
         print(fileName+' loading failed')

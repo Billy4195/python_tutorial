2bf65d8c733c69379680c324f6beee5c210ef089
Billy4195 <g4691821@gmail.com>
2016-02-29 19:59:09 +0800

    add progress

--- section.py
+++ section.py
@@ -1,6 +1,8 @@
 class section:
     def __init__(self,introduction):
-        self.introduction = introduction     
+        self.introduction = introduction
+        self.finished = 0
     def play(self):
         for line in self.introduction:
             print(line)
+        self.finished = 1
